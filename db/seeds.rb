664743b9cea23a47246ee1b786ebe0688272dc97
migo1 <ertmigot@gmail.com>
2023-09-28 18:12:46 +0300

    comments-posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,21 +7,29 @@
 #   Character.create(name: "Luke", movie: movies.first)
 require 'faker'
 
-10.times do |i|
-  User.create(
+20.times do |i|
+  user = User.create(
     name: Faker::Name.name,
     photo: "https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png",
-    bio: Faker::Lorem.paragraphs(number: 1).join("\n\n"),
-    posts_counter: 0
+    bio: Faker::Lorem.paragraphs(number: 4).join("\n\n"),
+    posts_counter: rand(5..15) # Random post count for each user (between 5 and 15)
+  )
+
+  user.posts_counter.times do
+    Post.create(
+      author: user, # Assign the user as the author
+      title: Faker::Lorem.sentence,
+      text: Faker::Lorem.paragraphs(number: 4).join("\n\n"),
+      likes_counter: rand(0..50),
+      comments_counter: rand(0..20)
+    )
+  end
+
+  50.times do
+  Comment.create(
+    user_id: User.order('RANDOM()').first.id, # Assign a random user as the commenter
+    post_id: Post.order('RANDOM()').first.id, # Assign a random post as the commented post
+    text: Faker::Lorem.sentence
   )
 end
-
-10.times do
-  Post.create(
-    author_id: rand(21..32), # Random author ID between 21 and 32
-    title: Faker::Lorem.sentence,
-    text: Faker::Lorem.paragraphs(number: 3).join("\n\n"), # Generates 3 paragraphs of text
-    likes_counter: rand(0..50), # Random likes counter
-    comments_counter: rand(0..20) # Random comments counter
-  )
 end
