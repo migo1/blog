4c11b6f2fc3737b0b2bd586fa1d78d1aba199708
migo1 <ertmigot@gmail.com>
2023-10-05 14:21:06 +0300

    add-api-routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,14 @@ Rails.application.routes.draw do
       resources :comments, only: [:new, :create, :destroy]
     end
   end
+
+    namespace :api, defaults: { format: 'json' } do
+    namespace :v1 do
+      resources :users do
+        resources :posts, only: [:index] do
+          resources :comments, only: [:index, :create]
+        end
+      end
+    end
+  end
 end
