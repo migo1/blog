e03e52af9ab49bfdf1f6c5647228f26422f9607a
migo1 <ertmigot@gmail.com>
2023-10-06 18:19:19 +0300

    add-test

--- spec/requests/api/v1/posts_spec.rb
+++ spec/requests/api/v1/posts_spec.rb
@@ -1,14 +1,16 @@
 require 'swagger_helper'
 
 RSpec.describe 'api/v1/posts', type: :request do
-
   path '/api/v1/users/{user_id}/posts' do
     # You'll want to customize the parameter types...
     parameter name: 'user_id', in: :path, type: :string, description: 'user_id'
-     
+    #    before do
+    #   @user = User.create(name: 'foo', email: 'user10@test.com', role: 'bar', posts_counter: 0)
+    # end
+
     get('list posts') do
       response(200, 'successful') do
-        let(:user_id) { '123' }
+        let(:user_id) { '362' }
 
         after do |example|
           example.metadata[:response][:content] = {
@@ -19,10 +21,10 @@ RSpec.describe 'api/v1/posts', type: :request do
         end
         run_test!
       end
-      response(404,'not found' ) do
-        let(:user_id) { '123' }
-        run_test!
-      end
+      # response(404,'Not found' ) do
+      #   let(:user_id) { '30' }
+      #   run_test!
+      # end
     end
   end
 end
